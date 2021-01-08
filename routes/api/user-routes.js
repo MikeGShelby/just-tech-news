f64c2b1015e9e58677c0d363ca072bf83c1caf46
Mike Shelby <mikegshelby@gmail.com>
2021-01-08 15:44:26 -0600

    create API routes for the post model

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// GET /api/users
+// GET /api/users (get all users)
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
     User.findAll({
