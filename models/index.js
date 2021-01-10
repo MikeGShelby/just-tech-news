961540293033043f1d90fe13d59c626b4f10a9a6
Mike Shelby <mikegshelby@gmail.com>
2021-01-10 07:31:43 -0600

    create Model for Comments, then update routes to include the model.

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
-// create associations
+// User/Post associations
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
@@ -11,13 +12,14 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
+// Vote associations
 User.belongsToMany(Post, {
     through: Vote,
     as: 'voted_posts',
     foreignKey: 'user_id'
 });
 
-  Post.belongsToMany(User, {
+Post.belongsToMany(User, {
     through: Vote,
     as: 'voted_posts',
     foreignKey: 'post_id'
@@ -39,4 +41,21 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+// Comment associations
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
