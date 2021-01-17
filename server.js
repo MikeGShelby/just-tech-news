cb876301003cc664010236f7642f5adaa14fd2ae
Mike Shelby <mikegshelby@gmail.com>
2021-01-17 17:11:50 -0600

    add functionality for upvotes and comments; conditionally render comment/upvote buttons and login links

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+
 app.use(session(sess));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
